397b254f95e6b3567e4f3a0ed707d8188019d2b7
Om-Alve <omalve8174@gmail.com>
2025-04-27 18:49:38 +0530

    fix bug in dataset

---
+++ dataset/dataset.py
@@ -0,0 +1,45 @@
+import json
+from torch.utils.data import Dataset
+import torch
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+class PretrainDataset(Dataset):
+    def __init__(self, dataset_path, tokenizer, max_length=512):
+        super().__init__()
+        self.dataset_path = dataset_path
+        self.tokenizer=tokenizer
+        self.max_length = max_length
+
+    def load_data(self, path):
+        samples = []
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                data = json.loads(line.strip())
+                samples.append(data)
+        return samples
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        sample = self.samples[index]
+
+        encoding = self.tokenizer(
+            str(sample["text"]),
+            max_length=self.max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+        
+        input_ids = encoding.input_ids.squeeze()
+        loss_mask = (input_ids != self.tokenizer.pad_token_id)
+
+        X = torch.tensor(input_ids[:-1], dtype=torch.long)
+        Y = torch.tensor(input_ids[1:], dtype=torch.long)
+
+        loss_mask = torch.tensor(loss_mask[1:], dtype=torch.long)
+
+        return X, Y, loss_mask
+
